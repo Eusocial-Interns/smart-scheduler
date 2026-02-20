f95020f268978f0356f7d401b287e774765ffbc7
gabrielbressanelli <gabriel.bressanelli@hotmail.com>
2026-02-20 14:16:05 -0500

    First Commit

---
+++ apps/scheduling/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+appname = 'scheduling'
+
+urlpatterns = [
+    path('dashboard/', views.dashboard, name='dashboard'),
+]
