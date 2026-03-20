830d58c32c37623bf4ae0f1f581b9a5ee2f068b4
Berro2424 <mhberro23@gmail.com>
2026-03-20 01:56:52 -0400

    Task 3: Implement basic scheduling API endpoints

--- apps/scheduling/views.py
+++ apps/scheduling/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from apps.scheduling.models import Employee, Shift, Assignment
+from .serializers import EmployeeSerializer, ShiftSerializer, AssignmentSerializer
 
-# Create your views here.
-def dashboard(request):
-    return render(request, 'scheduling/dashboard.html')
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+
+class ShiftViewSet(viewsets.ModelViewSet):
+    queryset = Shift.objects.all()
+    serializer_class = ShiftSerializer
+
+
+class AssignmentViewSet(viewsets.ModelViewSet):
+    queryset = Assignment.objects.all()
+    serializer_class = AssignmentSerializer
--- config/settings.py
+++ config/settings.py
@@ -44,9 +44,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'apps.messaging',
     'apps.scheduling',
-]
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
