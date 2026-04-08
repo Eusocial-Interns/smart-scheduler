9a42944cae424db2755e161acd6ec256716f13a8
Berro2424 <mhberro23@gmail.com>
2026-04-08 12:05:18 -0400

    Task 5: Availability system completed

--- apps/scheduling/admin.py
+++ apps/scheduling/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Employee, Role, Shift, Availability, Assignment
 
-# Register your models here.
+admin.site.register(Employee)
+admin.site.register(Role)
+admin.site.register(Shift)
+admin.site.register(Availability)
+admin.site.register(Assignment)
--- apps/scheduling/models.py
+++ apps/scheduling/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=100)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Availability(models.Model):
+    employee = models.ForeignKey("Employee", on_delete=models.CASCADE, related_name="availabilities")
+
+    date = models.DateField()
+
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+    STATUS_CHOICES = [
+        ("available", "Available"),
+        ("unavailable", "Unavailable"),
+        ("preferred", "Preferred"),
+    ]
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="available")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.employee} - {self.date} ({self.start_time} to {self.end_time})"
+
+
+class Shift(models.Model):
+    start_time = models.DateTimeField(null=True, blank=True)
+    end_time = models.DateTimeField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Assignment(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    shift = models.ForeignKey(Shift, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
